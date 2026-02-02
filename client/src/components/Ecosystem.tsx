1be76408e87320a26df091247fe92f2c5c246600
Faculdade LA Deploy <deploy@faculdade-la.local>
2026-02-01 23:48:41 -0500

    Auto-sync: 2026-02-01 23:48:41

--- client/src/components/Ecosystem.tsx
+++ client/src/components/Ecosystem.tsx
@@ -2,7 +2,7 @@ export default function Ecosystem() {
   return (
     <section id="ecossistema" className="w-full bg-white">
       <img 
-        src="/ecossistema.png" 
+        src="/ecossistema-organograma.png" 
         alt="Nosso Ecossistema - LA Educação" 
         className="w-full h-auto"
         loading="lazy"
